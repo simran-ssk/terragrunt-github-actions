d84a18442d0010aada408840c7aa98eb61232b27
Simran S. Khalsa <simransk@gmail.com>
2020-06-15 10:40:28 -0700

    Update terragrunt plan to plan-all

--- src/terragrunt_plan.sh
+++ src/terragrunt_plan.sh
@@ -3,7 +3,7 @@
 function terragruntPlan {
   # Gather the output of `terragrunt plan`.
   echo "plan: info: planning Terragrunt configuration in ${tfWorkingDir}"
-  planOutput=$(${tfBinary} plan -detailed-exitcode -input=false ${*} 2>&1)
+  planOutput=$(${tfBinary} plan-all -detailed-exitcode -input=false ${*} 2>&1)
   planExitCode=${?}
   planHasChanges=false
   planCommentStatus="Failed"
@@ -44,7 +44,7 @@ function terragruntPlan {
 
   # Comment on the pull request if necessary.
   if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${tfComment}" == "1" ] && ([ "${planHasChanges}" == "true" ] || [ "${planCommentStatus}" == "Failed" ]); then
-    planCommentWrapper="#### \`${tfBinary} plan\` ${planCommentStatus}
+    planCommentWrapper="#### \`${tfBinary} plan-all\` ${planCommentStatus}
 <details><summary>Show Output</summary>
 
 \`\`\`
